d1aac725137e678682bf1bb54c22c49d3ccb027a
vagrus <vagrus@users.noreply.github.com>
2015-11-29 19:31:00 +0400

    add handler

---
+++ src/Vagrus/Monolog/Handler/YiiArHandler.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Vagrus\Monolog\Handler;
+
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+/**
+ * Handler sending logs to Yii's AR model
+ *
+ * @package Vagrus\Monolog\Handler
+ */
+class YiiArHandler extends AbstractProcessingHandler
+{
+    /**
+     * @var string ActiveRecord model name
+     */
+    protected $modelName;
+    /**
+     * @var array context vars to AR model properties mapping settings
+     */
+    protected $mapping;
+
+    /**
+     * @param string $modelName
+     * @param string|array $mapping
+     * @param int $level
+     * @param bool $bubble
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($modelName, $mapping, $level = Logger::DEBUG, $bubble = true)
+    {
+        $this->modelName = $modelName;
+        $this->setMapping($mapping);
+
+        parent::__construct($level, $bubble);
+    }
+
+    /**
+     * @param string|array $mapping
+     * @throws \InvalidArgumentException if mapping not contain model property for log message
+     */
+    protected function setMapping($mapping)
+    {
+        if (is_string($mapping)) {
+            $mapping = array(
+                '*' => $mapping,
+            );
+        }
+
+        $mapping = (array)$mapping;
+
+        if (!array_key_exists('*', $mapping)) {
+            throw new \InvalidArgumentException("Mapping settings must contain AR model property");
+        }
+
+        $this->mapping = $mapping;
+    }
+
+    /**
+     * Writes the record down to the log of the implementing handler
+     *
+     * @param array $record
+     * @return void
+     */
+    protected function write(array $record)
+    {
+        $model = \CActiveRecord::model($this->modelName);
+        $logMessage = $record['formatted'];
+        $context = $record['context'];
+
+        foreach ($this->mapping as $key => $modelProperty) {
+            if ($key === '*') {
+                $model->setAttribute($modelProperty, $logMessage);
+            } elseif (array_key_exists($key, $context)) {
+                $model->setAttribute($modelProperty, $context[$key]);
+            }
+        }
+
+        $model->save(false);
+    }
+}
